48b57e688e4254c72659f0742ebf18ba44c94b33
Domenico Pagano <dpaganoh@gmail.com>
2025-12-16 10:42:41 -0500

    create: src/data / src/types + base form: Form.tsx

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,47 @@
+import { categories } from '../data/categories.ts'
+
 export default function Form() {
   return (
-    <div>Form</div>
+    <form className="space-y-5 bg-white shadow p-10 rounded-lg">
+        <div className="grid grid-cols-1 gap-3">
+            <label htmlFor="category" className="font-bold">Categoría:</label>
+            <select
+                id="category"
+                className="border border-slate-300 p-2 rounded-lg w-full bg-white"
+            >
+                {categories.map(category => (
+                    <option key={category.id} value={category.id} >
+                        {category.name}
+                    </option>
+                ))}
+            </select>
+        </div>
+
+        <div className="grid grid-cols-1 gap-3">
+            <label htmlFor="activity" className="font-bold">Actividad:</label>
+            <input 
+                id="activity"
+                type="text"
+                className="border border-slate-300 p-2 rounded-lg"
+                placeholder="Ej. Comida, Jungo de Naranja, Ensalada, Ejercicios, Pesas, Bicileta"
+            />
+        </div>
+
+        <div className="grid grid-cols-1 gap-3">
+            <label htmlFor="calories" className="font-bold">Calorías:</label>
+            <input 
+                id="calories"
+                type="number"
+                className="border border-slate-300 p-2 rounded-lg"
+                placeholder="Calorías. ej. 300 o 500"
+            />
+        </div>
+
+        <input 
+            type="submit"
+            className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
+            value='Guardar Comida o Guardar ejercicio'
+        />
+    </form>
   )
 }
