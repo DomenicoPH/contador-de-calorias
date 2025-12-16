fbf81103077e57698e0e66f45559b93d3e855821
Domenico Pagano <dpaganoh@gmail.com>
2025-12-16 16:51:06 -0500

    validation

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -17,6 +17,11 @@ export default function Form() {
         ...activity, 
         [e.target.id]: isNumberField ? +e.target.value : e.target.value 
     })
+  };
+
+  const isValidActivity = () => {
+    const {name, calories} = activity
+    return name.trim() !== '' && calories > 0
   }
 
   return (
@@ -69,8 +74,10 @@ export default function Form() {
 
       <input
         type="submit"
-        className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
+        className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer 
+                    disabled:opacity-10 disabled:cursor-auto"
         value="Guardar Comida o Guardar ejercicio"
+        disabled={!isValidActivity()}
       />
     </form>
   );
