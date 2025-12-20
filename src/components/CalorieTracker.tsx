c85b424527ee35fd54a1b07f2a18b569f254cb73
Domenico Pagano <dpaganoh@gmail.com>
2025-12-20 10:03:10 -0500

    Dynamic and reusable component: CalorieDisplay

--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -1,5 +1,6 @@
 import { useMemo } from "react";
 import type { Activity } from "../types";
+import CalorieDisplay from "./CalorieDisplay";
 
 type CalorieTrackerProps = {
   activities: Activity[];
@@ -13,21 +14,19 @@ export default function CalorieTracker({ activities }: CalorieTrackerProps) {
 
   return (
     <>
-      <h2 className="text-4xl font-black text-white text-center">
-        Resumen de Calorías
-      </h2>
+      <h2 className="text-4xl font-black text-white text-center">Resumen de Calorías</h2>
 
       <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10">
-        
-        <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
-            <span className="font-black text-6xl">{caloriesConsumed}</span>
-            Consumidas
-        </p>
 
-        <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
-            <span className="font-black text-6xl">{caloriesBurned}</span>
-            Quemadas
-        </p>
+        <CalorieDisplay 
+            calories={caloriesConsumed}
+            text='Consumidas'
+        />
+
+        <CalorieDisplay 
+            calories={caloriesBurned}
+            text='Quemadas'
+        />
 
       </div>
       
