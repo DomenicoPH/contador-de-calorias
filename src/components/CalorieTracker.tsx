e816a6275f90e551f05505e8c7d730237410b52c
Domenico Pagano <dpaganoh@gmail.com>
2026-01-07 16:56:21 -0500

    adaptando CalorieTracker al context

--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -1,41 +1,32 @@
-import { useMemo } from "react";
-import type { Activity } from "../types";
 import CalorieDisplay from "./CalorieDisplay";
+import { useActivity } from "../hooks/useActivity";
 
-type CalorieTrackerProps = {
-  activities: Activity[];
-};
+export default function CalorieTracker() {
 
-export default function CalorieTracker({ activities }: CalorieTrackerProps) {
-  
-    // Contadores
-    const caloriesConsumed = useMemo(() => activities.reduce((total, activity) => activity.category === 1 ? total + activity.calories : total, 0), [activities])
-    const caloriesBurned = useMemo(() => activities.reduce((total, activity) => activity.category === 2 ? total + activity.calories : total, 0), [activities])
-    const netCalories = useMemo(() => caloriesConsumed - caloriesBurned, [activities])
+  const { caloriesConsumed, caloriesBurned, netCalories } = useActivity();
 
   return (
     <>
-      <h2 className="text-4xl font-black text-white text-center">Resumen de Calorías</h2>
+      <h2 className="text-4xl font-black text-white text-center">
+        Resumen de Calorías
+      </h2>
 
       <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10">
-
         <CalorieDisplay 
-            calories={caloriesConsumed}
-            text='Consumidas'
+        calories={caloriesConsumed} 
+        text="Consumidas" 
         />
 
         <CalorieDisplay 
-            calories={caloriesBurned}
-            text='Quemadas'
+        calories={caloriesBurned} 
+        text="Quemadas" 
         />
 
         <CalorieDisplay 
-            calories={netCalories}
-            text='Totales'
+        calories={netCalories} 
+        text="Totales" 
         />
-
       </div>
-      
     </>
   );
 }
